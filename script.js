f77df12e77fac8df126d858c13027488caeac8cd
badge hunt <64038298+badgeminer@users.noreply.github.com>
2022-06-13 19:35:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var Game={};
+Game.count = 0
+Game.cps = 0
+function c() {
+  Game.count += 1
+  document.getElementById("count").innerHTML = Game.count;
+}
+
+
+
+
+
+
+while (true) {
+  // code block to be executed
+}
