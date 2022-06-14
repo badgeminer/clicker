27e74538be85e5a500c5959330b855afd7a9cbd8
badge hunt <64038298+badgeminer@users.noreply.github.com>
2022-06-14 20:29:29 +0000

    hjk

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var Game={};
 Game.count = 0
 Game.cps = 0
-Game.prs = 100
+Game.prs = 10000
 Game.prsL = 0
 Game.prsW = 128
 function c() {
@@ -9,16 +9,18 @@ function c() {
   draw()
 }
 function buy(cps,cost) {
+  if (cost <= Game.count) {
   Game.cps += cps
   Game.count -= cost
   draw()
+  }
 }
 
 function draw() {
   document.getElementById("cps").innerHTML = Game.cps;
   document.getElementById("count").innerHTML = Game.count;
-  document.getElementById("prs").width = (Game.count*Game.prs)/Game.prsW;
-  console.log((Game.count*Game.prs)/Game.prsW)
+  document.getElementById("prs").width = (Game.count/Game.prs)*Game.prsW;
+  //console.log((Game.count/Game.prs)*Game.prsW)
 }
 
 
