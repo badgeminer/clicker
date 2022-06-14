6ea197d12e57589735a1d0ba12ed6f8f9a7d9b19
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-06-14 07:52:46 -0600

    yh

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 var Game={};
 Game.count = 0
 Game.cps = 0
+Game.prs = 100
+Game.prsL = 0
+Game.prsW = 128
 function c() {
   Game.count += 1
-  document.getElementById("count").innerHTML = Game.count;
+  draw()
 }
 function buy(cps,cost) {
   Game.cps += cps
   Game.count -= cost
-  document.getElementById("cps").innerHTML = Game.count;
-  document.getElementById("count").innerHTML = Game.cps;
+  draw()
 }
 
+function draw() {
+  document.getElementById("cps").innerHTML = Game.cps;
+  document.getElementById("count").innerHTML = Game.count;
+  document.getElementById("prs").width = (Game.count*Game.prs)/Game.prsW;
+  console.log((Game.count*Game.prs)/Game.prsW)
+}
 
 
 
@@ -21,5 +29,5 @@ function start() {
 
 function tick() {
   Game.count += Game.cps
-  document.getElementById("count").innerHTML = Game.count;
+  draw()
 }
