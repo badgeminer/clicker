cac47a7904c31b5f2cb09b5add50ef80d88d3cf5
badgeminer <64038298+badgeminer@users.noreply.github.com>
2022-06-16 07:30:35 -0600

    nj
    k

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+var lang = "";
 var Game={};
 Game.count = 0
 Game.cps = 0
 Game.prs = 10000
 Game.prsL = 0
 Game.prsW = 128
+
+Game.itms = {}
+Game.itms.saw = {}
+Game.itms.saw.price = 2
+Game.itms.saw.cps = 1
+Game.itms.lumberjack = {}
+Game.itms.lumberjack.price = 100
+Game.itms.lumberjack.cps = 25
+
+
+
+Game.local = {}
+Game.local.upgrades = {}
+Game.local.upgrades.saw = "saw"
+Game.local.upgrades.lumberjack = "lumberjack"
+Game.local.log = "<img src=\"Log.png\" height=\"16\" width=\"16\">"
+
+
 function c() {
   Game.count += 1
   draw()
 }
-function buy(cps,cost) {
-  if (cost <= Game.count) {
-  Game.cps += cps
-  Game.count -= cost
+function buy(itm) {
+  if (Game.itms[itm].price <= Game.count) {
+  Game.cps += Game.itms[itm].cps
+  Game.count -= Game.itms[itm].price
+  Game.itms[itm].price += (Game.itms[itm].price/2)
   draw()
   }
 }
@@ -21,7 +41,7 @@ function draw() {
   document.getElementById("count").innerHTML = Game.count;
   document.getElementById("prs").width = (Game.count/Game.prs)*Game.prsW;
   //console.log((Game.count/Game.prs)*Game.prsW)
-  
+  refrShop()
 }
 
 
@@ -37,4 +57,9 @@ function tick() {
     
   }
   draw()
+}
+
+function refrShop() {
+  document.getElementById("saw").innerHTML = Game.local.upgrades.saw + " - " + Game.itms.saw.price +Game.local.log;
+  document.getElementById("lumberjack").innerHTML = Game.local.upgrades.lumberjack + " - " + Game.itms.lumberjack.price +Game.local.log;
 }
